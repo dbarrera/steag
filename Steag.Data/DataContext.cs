81580135331a5ea9bf5849431e9566a2239f2a72
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-04-13 19:40:52 +0800

    added DataContext

---
+++ Steag.Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Linq.Mapping;
+
+namespace Steag.Data
+{
+    internal class DataContext: System.Data.Linq.DataContext
+    {
+        public DataContext(string connectionString)
+            :base(connectionString)
+        {
+            
+        }
+
+        public DataContext(IDbConnection connection)
+            :base(connection)
+        {
+            
+        }
+
+        public DataContext(IDbConnection connection, MappingSource source)
+            :base(connection, source)
+        {
+            
+        }
+    }
+}
--- Steag.Data/DataSession.cs
+++ Steag.Data/DataSession.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
 using Steag.Framework.Authentication;
 
 namespace Steag.Data
 {
     public abstract class DataSession
     {
+        private DataContext _dataContext;
+
         public virtual User CurrentUser { get; protected set; }
 
         private Framework.Event.EventDispatcher EventDispatcher
@@ -23,6 +26,20 @@ namespace Steag.Data
         protected void RaiseEvent(string eventName, object sender, EventArgs e)
         {
             EventDispatcher.RaiseEvent(eventName, sender, e);    
+        }
+
+        protected virtual DataContext DataContext
+        {
+            get
+            {
+                if(Equals(_dataContext, null))
+                {
+                    var connectionStringKey = "";
+                    var connectionString = ConfigurationManager.ConnectionStrings[connectionStringKey];
+
+                }
+                return _dataContext;
+            }
         }
 
         protected DataSession(User user)
