3bc1d91ae316c5a85e84edb8af73d8ed65620935
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-04-17 23:36:31 +0800

    Modified Database Project

--- Steag.Database/Schema Objects/Schemas/dbo/Tables/UserAccount.table.sql
+++ Steag.Database/Schema Objects/Schemas/dbo/Tables/UserAccount.table.sql
@@ -1,5 +1,34 @@
-﻿CREATE TABLE [dbo].[UserAccount]
+﻿CREATE TABLE [dbo].[UserAccount](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[UserName] [nvarchar](60) NOT NULL,
+	[Password] [nvarchar](128) NOT NULL,
+	[Salt] [nvarchar](128) NOT NULL,
+	[UserRoleID] [bigint] NOT NULL,
+	[FirstName] [nvarchar](250) NULL,
+	[MiddleName] [nvarchar](250) NULL,
+	[LastName] [nvarchar](250) NULL,
+	[EmailAddress] [nvarchar](250) NULL,
+	[IsActive] [bit] NULL,
+	[UserCreated] [bigint] NULL,
+	[DateCreated] [datetime] NULL,
+	[UserModified] [bigint] NULL,
+	[DateModified] [datetime] NULL,
+ CONSTRAINT [PK_UserAccount] PRIMARY KEY CLUSTERED 
 (
-	ID bigint PRIMARY KEY, 
-	column_2 int NULL
-)
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[UserAccount]  WITH CHECK ADD  CONSTRAINT [FK_UserAccount_UserRole] FOREIGN KEY([UserRoleID])
+REFERENCES [dbo].[UserRole] ([ID])
+GO
+
+ALTER TABLE [dbo].[UserAccount] CHECK CONSTRAINT [FK_UserAccount_UserRole]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'UserRole FK' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'UserAccount', @level2type=N'CONSTRAINT',@level2name=N'FK_UserAccount_UserRole'
+GO
+
+
