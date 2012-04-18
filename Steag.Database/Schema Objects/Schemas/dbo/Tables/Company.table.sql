4bd49ed0e9cb17c8f9828626f84828337546ccd9
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-04-19 00:49:23 +0800

    modified database project

---
+++ Steag.Database/Schema Objects/Schemas/dbo/Tables/Company.table.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[Company](
+	[ID] [bigint] NOT NULL,
+	[CompanyName] [nvarchar](250) NOT NULL,
+	[Address1] [nvarchar](250) NULL,
+	[Address2] [nvarchar](250) NULL,
+	[State] [nvarchar](250) NULL,
+	[Country] [nvarchar](250) NULL,
+	[PostalCode] [nvarchar](250) NULL,
+	[EmailAddress2] [nvarchar](250) NULL,
+	[EmailAddrses2] [nvarchar](250) NULL,
+	[ContactPerson] [bigint] NULL,
+	[TelNo1] [nvarchar](15) NULL,
+	[TelNo2] [nvarchar](15) NULL,
+	[IsActive] [bit] NULL,
+	[UserCreated] [bigint] NULL,
+	[DateCreated] [datetime] NULL,
+	[UserModified] [bigint] NULL,
+	[DateModified] [datetime] NULL,
+ CONSTRAINT [PK_Company] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
---
+++ Steag.Database/Schema Objects/Schemas/dbo/Tables/UserRoleWebPage.table.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE [dbo].[UserRoleWebPage](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[UserRoleID] [bigint] NOT NULL,
+	[WebPageID] [bigint] NOT NULL,
+	[IsActive] [bit] NULL,
+	[UserCreated] [bigint] NULL,
+	[DateCreated] [datetime] NULL,
+	[UserModified] [bigint] NULL,
+	[DateModififed] [datetime] NULL,
+ CONSTRAINT [PK_UserRoleWebPage] PRIMARY KEY CLUSTERED 
+(
+	[UserRoleID] ASC,
+	[WebPageID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[UserRoleWebPage]  WITH CHECK ADD  CONSTRAINT [FK_UserRoleWebPage_UserRole] FOREIGN KEY([UserRoleID])
+REFERENCES [dbo].[UserRole] ([ID])
+GO
+
+ALTER TABLE [dbo].[UserRoleWebPage] CHECK CONSTRAINT [FK_UserRoleWebPage_UserRole]
+GO
+
+ALTER TABLE [dbo].[UserRoleWebPage]  WITH CHECK ADD  CONSTRAINT [FK_UserRoleWebPage_WebPage] FOREIGN KEY([WebPageID])
+REFERENCES [dbo].[WebPageRegistry] ([ID])
+GO
+
+ALTER TABLE [dbo].[UserRoleWebPage] CHECK CONSTRAINT [FK_UserRoleWebPage_WebPage]
+GO
